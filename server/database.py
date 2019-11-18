d90098b49772197d330e19b84040fbc4f557e586
isshun226 <bjsi1290346@gn.iwasaki.ac.jp>
2019-11-18 23:16:03 +0900

    fix sql syntax and add method to commit the sql

--- server/database.py
+++ server/database.py
@@ -2,10 +2,33 @@ import mysql.connector as mydb
 
 
 class Database:
-    def __init__(self, host: str, port: str, user: str, password: str, database: str):
-        self.conn = mydb.connect(
-            host=host, port=port, user=user, password=password, database=database,
-        )
-        self.conn.ping(reconnect=True)
-        self.cursor = self.conn.cursor()
-        print(self.conn.is_connected())
+	def __init__(self, host: str, port: str, user: str, password: str, database: str):
+		self.conn = mydb.connect(
+			host=host, port=port, user=user, password=password, database=database,
+		)
+		self.conn.ping(reconnect=True)
+
+		self.cursor = self.conn.cursor()
+
+		print(self.conn.is_connected())
+
+		self.cursor.execute(
+			"""
+			CREATE TABLE IF NOT EXISTS user(
+				student_id        INT(7) NOT NULL,
+				name              CHAR(30) NOT NULL,
+				department        CHAR(20) NOT NULL,
+				PRIMARY KEY(`student_id`)
+			)
+			"""
+		)
+		self.conn.commit()
+
+	def register_member(self, student_id, name, department):
+		self.cursor.execute(
+			"""
+			INSERT INTO user VALUES (%s, %s, %s);
+			""",
+			[student_id, name, department]
+		)
+		self.conn.commit()
--- server/main.py
+++ server/main.py
@@ -1,7 +1,8 @@
 import database
 
 
-#if __name__ == "__main__":
-mydb = database.Database(
-    host="localhost", port="3306", user="root", password="makeit", database="member"
-)
+if __name__ == "__main__":
+	mydb = database.Database(
+		host="localhost", port="3306", user="root", password="root", database="member"
+  )
+	
