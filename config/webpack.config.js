b67e2729807166e369bd747520c03d7d9cde9682
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-12-07 14:41:51 +0900

    :new:ADD src/client
    ~

---
+++ config/webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: "./src/client/index.html",
+  filename: "./index.html"
+});
+
+module.exports = module.exports = {
+  entry: "./src/client/index.js",
+  output: {
+    path: path.resolve("dist"),
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.js$|\.jsx$)/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [htmlWebpackPlugin],
+  resolve: {
+    extensions: [".wasm", ".mjs", ".js", ".json", ".jsx"],
+    modules: [path.resolve(__dirname, "src"), "node_modules"]
+  }
+};
